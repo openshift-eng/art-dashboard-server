87e4c0cedb36a492da9e1cc14693ffa36616f6c9
Adarsh Trivedi <adtrived@adtrived.remote.csb>
2020-06-10 12:33:56 -0400

    Setting.py removed db configuration. Not required.

--- build_interface/settings.py
+++ build_interface/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'build_interface.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
