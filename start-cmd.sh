3348ac095d3a4bbb6beeae9d5bd71498628db4d9
Ashwin Das <ashwindasr@gmail.com>
2022-08-05 09:27:27 -0400

    Advisory data collection fix (#24)
    * build history filter fix
    
    * fix advisory issue
    
    * remove database logic
    
    * support for 3.11, 4.5
    
    * delete migration file

--- start-cmd.sh
+++ start-cmd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export RUN_ENV=development
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8080 --noreload
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8080 --noreload
