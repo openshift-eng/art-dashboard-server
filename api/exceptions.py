a975027a4f4786e27b1ca62ac861e72884ddcf38
Ashwin Das <ashwindasr@gmail.com>
2022-11-10 11:31:43 -0500

    rename from artbot to artdash

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,103 +1,103 @@
-class ArtBotExceptions(Exception):
+class ArtDashExceptions(Exception):
     """Super class for all ART bot exceptions"""
     pass
 
 
 # Art bot exceptions
-class DistgitNotFound(ArtBotExceptions):
+class DistgitNotFound(ArtDashExceptions):
     """Exception raised for errors in the input dist-git name."""
     pass
 
 
-class CdnFromBrewNotFound(ArtBotExceptions):
+class CdnFromBrewNotFound(ArtDashExceptions):
     """Exception raised if CDN is not found from brew name and variant"""
     pass
 
 
-class CdnNotFound(ArtBotExceptions):
+class CdnNotFound(ArtDashExceptions):
     """Exception raised if CDN is not found from CDN name"""
     pass
 
 
-class DeliveryRepoNotFound(ArtBotExceptions):
+class DeliveryRepoNotFound(ArtDashExceptions):
     """Exception raised if delivery repo not found"""
     pass
 
 
-class BrewIdNotFound(ArtBotExceptions):
+class BrewIdNotFound(ArtDashExceptions):
     """Exception raised if brew id not found for the given brew package name"""
     pass
 
 
-class VariantIdNotFound(ArtBotExceptions):
+class VariantIdNotFound(ArtDashExceptions):
     """Exception raised if variant id not found for a CDN repo"""
     pass
 
 
-class CdnIdNotFound(ArtBotExceptions):
+class CdnIdNotFound(ArtDashExceptions):
     """Exception raised if CDN id not found for a CDN repo"""
     pass
 
 
-class ProductIdNotFound(ArtBotExceptions):
+class ProductIdNotFound(ArtDashExceptions):
     """Exception raised if Product id not found for a product variant"""
     pass
 
 
-class DeliveryRepoUrlNotFound(ArtBotExceptions):
+class DeliveryRepoUrlNotFound(ArtDashExceptions):
     """Exception raised if delivery repo not found on Pyxis."""
     pass
 
 
-class DeliveryRepoIDNotFound(ArtBotExceptions):
+class DeliveryRepoIDNotFound(ArtDashExceptions):
     """Exception raised if delivery repo ID not found on Pyxis."""
     pass
 
 
-class GithubFromDistgitNotFound(ArtBotExceptions):
+class GithubFromDistgitNotFound(ArtDashExceptions):
     """Exception raised if GitHub repo could not be found from the distgit name"""
     pass
 
 
-class DistgitFromGithubNotFound(ArtBotExceptions):
+class DistgitFromGithubNotFound(ArtDashExceptions):
     """Exception raised if Distgit repo could not be found from the GitHub repo name"""
     pass
 
 
-class MultipleCdnToBrewMappings(ArtBotExceptions):
+class MultipleCdnToBrewMappings(ArtDashExceptions):
     """Exception raised if more than one Brew packages are found for a CDN repo"""
     pass
 
 
-class BrewNotFoundFromCdnApi(ArtBotExceptions):
+class BrewNotFoundFromCdnApi(ArtDashExceptions):
     """Exception raised when the json file returned from the CDN repos API does not have Brew packages listed"""
     pass
 
 
-class BrewFromDeliveryNotFound(ArtBotExceptions):
+class BrewFromDeliveryNotFound(ArtDashExceptions):
     """Exception raised when the brew name could not be retrieved from pyxis"""
     pass
 
 
-class MultipleBrewFromDelivery(ArtBotExceptions):
+class MultipleBrewFromDelivery(ArtDashExceptions):
     """Exception raised if more than one delivery to brew mappings found"""
     pass
 
 
-class BrewToCdnWithDeliveryNotFound(ArtBotExceptions):
+class BrewToCdnWithDeliveryNotFound(ArtDashExceptions):
     """Exception raised when we cannot found the CDN repo name using the Brew name that we got using the Delivery Repo name"""
     pass
 
 
-class DistgitFromBrewNotFound(ArtBotExceptions):
+class DistgitFromBrewNotFound(ArtDashExceptions):
     """Exception raised when we cannot find the distgit name from the given brew name"""
 
 
-class NullDataReturned(ArtBotExceptions):
+class NullDataReturned(ArtDashExceptions):
     """Exception raise when null data (empty dict, list etc.) is returned from any function, which is necessary for other functions"""
 
 
-class BrewToDistgitMappingNotFound(ArtBotExceptions):
+class BrewToDistgitMappingNotFound(ArtDashExceptions):
     """Exception raised when no mapping is found between brew and distgit from the yml file from ocp-build-data/images"""
 
 
