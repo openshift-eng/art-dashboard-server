b5fc3dfef751f37c6c04ef163c207bd93ce2ae88
Ashwin Das <ashwindasr@gmail.com>
2023-01-02 17:04:01 -0500

    update keytab file path

--- lib/errata/kerberos.py
+++ lib/errata/kerberos.py
@@ -1,5 +1,6 @@
-from build_interface.settings import BASE_DIR
 import subprocess
+
+KEYTAB_FILE_PATH = "/tmp/keytab/redhat.keytab"
 
 
 def handle_kinit():
@@ -7,10 +8,7 @@ def handle_kinit():
     Funtion performs kinit
     :return: None
     """
-
-    keytab_file = BASE_DIR + "/.keytab/redhat.keytab"
-    print(keytab_file)
-    kinit_request = subprocess.Popen(["kinit", "-kt", keytab_file, "ocp-readonly/psi.redhat.com@REDHAT.COM"],
+    kinit_request = subprocess.Popen(["kinit", "-kt", KEYTAB_FILE_PATH, "ocp-readonly/psi.redhat.com@REDHAT.COM"],
                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = kinit_request.communicate()
     if error:
